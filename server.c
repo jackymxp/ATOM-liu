173276595297a71bf5f4708265ffaeb55516f8e5
jackymxp <53531417+jackymxp@users.noreply.github.com>
2021-12-31 20:11:58 -0800

    Update server.c

--- server.c
+++ server.c
@@ -17,10 +17,13 @@
 #include <sys/socket.h>
 #include <float.h>
 
+/* You will also need to add #include for your graph library header files */
 #include "graph.h"
 #include "dijkstra.h"
 
-/* You will also need to add #include for your graph library header files */
+// for debug
+#define COMMAND_GRAPH_SHOW          "GRAPH-SHOW"
+
 #define NDEBUG
 #ifdef NDEBUG
 #define debug(M, ...)
@@ -28,6 +31,7 @@
 #define debug(M, ...) fprintf( stderr, "DEBUG %s: %d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
+// command
 #define COMMAND_QUIT                "QUIT"
 #define COMMAND_NET_ADD             "NET-ADD"
 #define COMMAND_NET_DELETE          "NET-DELETE"
@@ -40,27 +44,222 @@
 
 #define MESSAGE_SIZE 512
 
+static void graph_show(Graph* graph, char* read_message, char* write_message) {
+    char command[MESSAGE_SIZE];
+    debug("GRAPH-SHOW\n");
+    print_graph(graph);
+
+    sprintf(write_message, "%s\n", "+OK GraphShow");
+}
+static void net_add(Graph* graph, char* read_message, char* write_message) {
+    char command[MESSAGE_SIZE];
+    int nid = -1;
+    debug("NET-ADD\n");
+    sscanf(read_message, "%s %d", command, &nid);
+    debug("command : %s nid %d\n", command, nid);
+    // net id > 0
+    if (nid <= 0) {
+        sprintf(write_message, "%s\n", "-ERR Added: NetID ");
+        return ;
+    } 
+    if (find_vertex(graph, nid) != NULL) {
+        sprintf(write_message, "%s %d %s\n", "-ERR Added", nid, "Has EXISTED");
+        return ;
+    }
+    add_vertex(graph, nid);
+    sprintf(write_message, "%s %d\n", "+OK Added", nid);
+}
+
+
+static void net_delete(Graph* graph, char* read_message, char* write_message) {
+    char command[MESSAGE_SIZE];
+    int nid = -1;
+    debug("NET-DELETE\n");
+    sscanf(read_message, "%s %d", command, &nid);
+    debug("command : %s nid %d\n", command, nid);
+    // net id > 0
+    if (nid <= 0) {
+        sprintf(write_message, "%s\n", "-ERR Deleted: NetID ");
+        return ;
+    }
+    if (find_vertex(graph, nid) == NULL) {
+        sprintf(write_message, "%s %d %s\n", "-ERR Deleted", nid, "No EXISTED");
+        return ;
+    }
+    remove_vertex(graph, nid);
+    sprintf(write_message, "%s %d\n", "+OK Deleted", nid);
+}
+
+static void route_add(Graph* graph, char* read_message, char* write_message) {
+    int from = -1, to = -1, nid = -1;
+    double weight = 0.0;
+    char command[MESSAGE_SIZE];
+    Edge* ft = NULL, *tf = NULL;
+
+    debug("ROUTE-ADD\n");
+    sscanf(read_message, "%s %d %d %lf", command, &from, &to, &weight);
+    debug("command : %s from %d to %d weight %f\n", command, from, to, weight);
+    if (from <= 0 || to <= 0 || weight <= 0) {
+        sprintf(write_message, "%s\n", "-ERR ROUTE-ADD");
+        return ;
+    } 
+    if (find_vertex(graph, from) == NULL || find_vertex(graph, to) == NULL) {
+        sprintf(write_message, "%s %d %d %s\n", "-ERR ROUTE-ADD", from, to, " NOT EXISTED");
+        return ;
+    } 
+    ft = get_edge(graph, from, to);
+    tf = get_edge(graph, to, from);
+    if (ft == NULL && tf == NULL) {
+        add_edge_undirected(graph, from, to, weight);
+        sprintf(write_message, "%s %d %s %d %s %lf\n", "+OK ROUTE-ADD:", from, " -> ", to, " : ",
+                weight);
+    } else if(ft != NULL && tf != NULL){
+        debug("the route has exist from %lf to %lf\n", ft->weight, weight);
+        sprintf(write_message, "%s %d %s %d %s %lf %s %lf\n", "+OK ROUTE-ADD EXISTED ", from, " -> ",
+                to, " : ", ft->weight, " -->> ", weight);
+        ft->weight = weight;
+        tf->weight = weight;
+    } else {
+        sprintf(write_message, "%s\n", "-ERR : ROUTE-ADD: MUST NOT OCCUR");
+    }
+}
+
+
+static void route_delete(Graph* graph, char* read_message, char* write_message) {
+    int from = -1, to = -1 ,nid = -1;
+    char command[MESSAGE_SIZE];
+    debug("ROUTE-DELETE\n");
+    sscanf(read_message, "%s %d %d", command, &from, &to);
+    debug("command : %s from %d to %df\n", command, from, to);
+    if (from <= 0 || to <= 0) {
+        sprintf(write_message, "%s\n", "-ERR ROUTE-DELETE: NetID");
+        return ;
+    }
+
+    if (!get_edge(graph, from, to) || !get_edge(graph, to, from)) {
+        sprintf(write_message, "%s %d <-> %d %s\n", "-ERR ROUTE-DELETE", from, to, "NOT EXIST");
+        return ;
+    } 
+    remove_edge(graph, from, to);
+    remove_edge(graph, to, from);
+    sprintf(write_message, "%s: %d <-> %d\n", "+OK ROUTE-DELETE", from, to);
+}
+
+static void route_show(Graph* graph, char* read_message, char* write_message) {
+    int nid = -1,  nidCount = 0;
+    Edge** pEdges = NULL;
+    char command[MESSAGE_SIZE];
+    char tmp[MESSAGE_SIZE];
+    Vertex *vertex;
+
+    debug("ROUTE-SHOW\n");
+    sscanf(read_message, "%s %d", command, &nid);
+    debug("command : %s nid %d\n", command, nid);
+    // net id > 0
+    if (nid <= 0) {
+        sprintf(write_message, "%s\n", "-ERR ROUTE-SHOW: NetID ");
+        return;
+    } 
+    if ((vertex = find_vertex(graph, nid)) == NULL) {
+        sprintf(write_message, "%s\n", "-ERR ROUTE-SHOW: NetID NOT EXISTED");
+        return ;
+    }
+
+    pEdges = get_edges(graph, vertex, &nidCount);
+    sprintf(write_message, "%s %d\n", "+OK ROUTE-SHOW", nidCount);
+    for (int i = 0; i < nidCount; i++) {
+        sprintf(tmp, "%d\n", pEdges[i]->vertex->id);
+        strcat(write_message, tmp);
+    }
+    // free edges
+    free(pEdges);
+}
+
+
+static void net_list(Graph* graph, char* read_message, char* write_message) {
+    int* ids;
+    int nidCount = 0, i = 0;
+    char command[MESSAGE_SIZE];
+    char tmp[MESSAGE_SIZE];
+    debug("NET-LIST\n");
+    ids = get_vertices(graph, &nidCount);
+    sprintf(write_message, "%s %d\n", "+OK", nidCount);
+    for (i = 0; i < nidCount; i++) {
+        debug("  %d\n", ids[i]);
+        sprintf(tmp, "%d\n", ids[i]);
+        strcat(write_message, tmp);
+    }
+    // free netId
+    free(ids);
+}
+
+static void route_hop(Graph* graph, char* read_message, char* write_message) {
+    int from = -1, to = -1, nidCount = 0;
+    char command[MESSAGE_SIZE];
+    Path* path;
+    debug("ROUTE-HOP");
+    sscanf(read_message, "%s %d %d", command, &from, &to);
+    debug("command : %s from %d to %d\n", command, from, to);
+    if (from <= 0 || to <= 0) {
+        sprintf(write_message, "%s\n", "-ERR ROUTE-HOP:NetID");
+        return ;
+    } 
+    if (from == to || find_vertex(graph, from) == NULL || find_vertex(graph, to) == NULL) {
+        sprintf(write_message, "%s %d %d\n", "-ERR ROUTE-HOP", from, to);
+        return ;
+    } 
+    path = dijkstra(graph, from, &nidCount);
+    sprintf(write_message, "%s %d %.2f\n", "+OK ROUTE-HOP", path[to].next_hop, path[to].weight);
+    free(path);
+}
+
+static void route_table(Graph* graph, char* read_message, char* write_message) {
+    int nid = 0, nidCount = 0, i = 0;
+    Path* path;
+    int* ids;
+    int pnEntry = 0;
+    char tmp[MESSAGE_SIZE];
+    char command[MESSAGE_SIZE];
+    debug(COMMAND_ROUTE_TABLE);
+    sscanf(read_message, "%s %d", command, &nid);
+    debug("command : %s nid %d\n", command, nid);
+    // net id > 0
+    if (nid <= 0) {
+        sprintf(write_message, "%s\n", "-ERR ROUTE-TABLE: NetID ");
+        return ;
+    } 
+    path = dijkstra(graph, nid, &pnEntry);
+    ids = get_vertices(graph, &nidCount);
+
+    sprintf(write_message, "%s %d\n", "+OK ROUTE-TABLE", nid);
+
+    for (i = 0; i < nidCount; i++) {
+        Path *p = path + ids[i];
+        debug("%d\n", ids[i]);
+        if(nid == ids[i]) continue;
+        if(p->weight != DBL_MAX)
+            sprintf(tmp, "%d -> %d, next-hop %d, weight %8.2f\n", nid, ids[i], p->next_hop, p->weight);
+        else
+            sprintf(tmp, "%d -> %d, next-hop %d, weight     INF\n", nid, ids[i], p->next_hop);
+        strcat(write_message, tmp);
+    }
+    free(ids);
+    free(path);
+}
+
+
+
 int main(int argc, const char *argv[]) {
     int serverSocket, clientSocket;
     struct sockaddr_in serv_addr;
     struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_size;
 
-
     char connect_message[] = "+OK CONNECTED\n";
     char command[MESSAGE_SIZE];
-    char *p;
     char write_message[MESSAGE_SIZE];
     char read_message[MESSAGE_SIZE];
-    int nid, from, to, nidCount, i;
-    double weight;
-    Edge *edge;
-    int *ids;
-    char tmp[128];
-    Vertex *vertex;
-    Edge **pEdges;
-    Path *path;
-    int pnEntry = 0;
+
     printf("Programming Portfolio 2021 Implementation\n");
 
     /* Insert your code here to create, and the TCP/IP socket for the Network Server
@@ -97,7 +296,7 @@ int main(int argc, const char *argv[]) {
 
     clnt_addr_size = sizeof(clnt_addr);
     if ((clientSocket = accept(serverSocket, (
-            struct sockaddr *) &clnt_addr, &clnt_addr_size)) == -1) {
+                        struct sockaddr *) &clnt_addr, &clnt_addr_size)) == -1) {
         fprintf(stderr, "accept error\n");
         exit(EXIT_FAILURE);
     }
@@ -113,167 +312,35 @@ int main(int argc, const char *argv[]) {
 
     while (1) {
         read(clientSocket, read_message, MESSAGE_SIZE);
-        nidCount = 0;
 
         debug("read message %s\n", read_message);
         if (strstr(read_message, COMMAND_QUIT)) {
-            debug("接收到退出命令\n");
-            write(clientSocket, "+OK\n", sizeof("+OK\n"));
             break;
         } else if (strstr(read_message, COMMAND_ROUTE_TABLE)) {
-            debug(COMMAND_ROUTE_TABLE);
-            nid = 0;
-            nidCount = 0;
-            sscanf(read_message, "%s %d", command, &nid);
-            debug("command : %s nid %d\n", command, nid);
-            // net id > 0
-            if (nid <= 0) {
-                sprintf(write_message, "%s\n", "-ERR ROUTE-TABLE: NetID ");
-            } else {
-                path = dijkstra(graph, nid, &pnEntry);
-                ids = get_vertices(graph, &nidCount);
-
-                sprintf(write_message, "%s %d\n", "+OK ROUTE-TABLE", nid);
-
-                for (i = 0; i < nidCount; i++) {
-                    Path *p = path + ids[i];
-                    debug("%d\n", ids[i]);
-                    if(nid == ids[i]) continue;
-                    if(p->weight != DBL_MAX)
-                        sprintf(tmp, "%d -> %d, next-hop %d, weight %8.2f\n", nid, ids[i], p->next_hop, p->weight);
-                    else
-                        sprintf(tmp, "%d -> %d, next-hop %d, weight     INF\n", nid, ids[i], p->next_hop);
-                    strcat(write_message, tmp);
-                }
-            }
-
+            route_table(graph, read_message, write_message);
         } else if (strstr(read_message, COMMAND_ROUTE_HOP)) {
-            debug("ROUTE-HOP");
-            from = 0, to = -1;
-            sscanf(read_message, "%s %d %d", command, &from, &to);
-            debug("command : %s from %d to %d\n", command, from, to);
-            if (from <= 0 || to <= 0) {
-                sprintf(write_message, "%s\n", "-ERR ROUTE-HOP:NetID");
-            } else {
-                if (from == to || find_vertex(graph, from) == NULL || find_vertex(graph, to) == NULL) {
-                    sprintf(write_message, "%s %d %d\n", "-ERR ROUTE-HOP", from, to);
-                } else {
-                    path = dijkstra(graph, from, &nidCount);
-                    sprintf(write_message, "%s %d %.2f\n", "+OK ROUTE-HOP", path[to].next_hop, path[to].weight);
-                    free(path);
-                }
-            }
+            route_hop(graph, read_message, write_message);
         } else if (strstr(read_message, COMMAND_NET_LIST)) {
-            debug("NET-LIST\n");
-            ids = get_vertices(graph, &nidCount);
-            sprintf(write_message, "%s %d\n", "+OK", nidCount);
-            for (i = 0; i < nidCount; i++) {
-                debug("  %d\n", ids[i]);
-                sprintf(tmp, "%d\n", ids[i]);
-                strcat(write_message, tmp);
-            }
-            // free netId
-            free(ids);
+            net_list(graph, read_message, write_message);
         } else if (strstr(read_message, COMMAND_ROUTE_SHOW)) {
-            debug("ROUTE-SHOW\n");
-            nid = 0;
-            sscanf(read_message, "%s %d", command, &nid);
-            debug("command : %s nid %d\n", command, nid);
-            // net id > 0
-            if (nid <= 0) {
-                sprintf(write_message, "%s\n", "-ERR ROUTE-SHOW: NetID ");
-            } else {
-                if ((vertex = find_vertex(graph, nid)) == NULL) {
-                    sprintf(write_message, "%s\n", "-ERR ROUTE-SHOW: NetID NOT EXISTED");
-                } else {
-                    pEdges = get_edges(graph, vertex, &nidCount);
-                    sprintf(write_message, "%s %d\n", "+OK ROUTE-SHOW", nidCount);
-                    for (int i = 0; i < nidCount; i++) {
-                        sprintf(tmp, "%d\n", pEdges[i]->vertex->id);
-                        strcat(write_message, tmp);
-                    }
-                }
-            }
-        } else if ((strstr(read_message, COMMAND_NET_ADD)) != NULL) {
-            debug("NET-ADD\n");
-            nid = 0;
-            sscanf(read_message, "%s %d", command, &nid);
-            debug("command : %s nid %d\n", command, nid);
-            // net id > 0
-            if (nid <= 0) {
-                sprintf(write_message, "%s\n", "-ERR Added: NetID ");
-            } else {
-                if (find_vertex(graph, nid) == NULL) {
-                    add_vertex(graph, nid);
-                    sprintf(write_message, "%s %d\n", "+OK Added", nid);
-                } else {
-                    sprintf(write_message, "%s %d %s\n", "-ERR Added", nid, "Has EXISTED");
-                }
-            }
-        } else if ((p = strstr(read_message, COMMAND_NET_DELETE)) != NULL) {
-            debug("NET-DELETE\n");
-            nid = 0;
-            sscanf(read_message, "%s %d", command, &nid);
-            debug("command : %s nid %d\n", command, nid);
-            // net id > 0
-            if (nid <= 0) {
-                sprintf(write_message, "%s\n", "-ERR Deleted: NetID ");
-            } else {
-                if (find_vertex(graph, nid) != NULL) {
-                    remove_vertex(graph, nid);
-                    sprintf(write_message, "%s %d\n", "+OK Deleted", nid);
-
-                } else {
-                    sprintf(write_message, "%s %d %s\n", "-ERR Deleted", nid, "No EXISTED");
-                }
-            }
-
-        } else if ((p = strstr(read_message, COMMAND_ROUTE_ADD)) != NULL) {
-            debug("ROUTE-ADD\n");
-            from = 0, to = 0, weight = 0.0;
-            sscanf(read_message, "%s %d %d %lf", command, &from, &to, &weight);
-            debug("command : %s from %d to %d weight %f\n", command, from, to, weight);
-            if (from <= 0 || to <= 0 || weight <= 0) {
-                sprintf(write_message, "%s\n", "-ERR ROUTE-ADD");
-            } else {
-                if (find_vertex(graph, from) == NULL || find_vertex(graph, to) == NULL) {
-                    sprintf(write_message, "%s %d %d %s\n", "-ERR ROUTE-ADD", from, to, " NOT EXISTED");
-                } else {
-                    if ((edge = get_edge(graph, from, to)) == NULL) {
-                        add_edge_undirected(graph, from, to, weight);
-                        sprintf(write_message, "%s %d %s %d %s %lf\n", "+OK ROUTE-ADD:", from, " -> ", to, " : ",
-                                weight);
-                    } else {
-                        debug("the route has exist from %lf to %lf\n", edge->weight, weight);
-                        sprintf(write_message, "%s %d %s %d %s %lf %s %lf\n", "+OK ROUTE-ADD EXISTED ", from, " -> ",
-                                to, " : ", edge->weight, " -->> ", weight);
-                        edge->weight = weight;
-                    }
-                }
-            }
-
-        } else if ((p = strstr(read_message, COMMAND_ROUTE_DELETE)) != NULL) {
-            debug("ROUTE-DELETE\n");
-            from = 0, to = 0;
-            sscanf(read_message, "%s %d %d", command, &from, &to);
-            debug("command : %s from %d to %df\n", command, from, to);
-            if (from <= 0 || to <= 0) {
-                sprintf(write_message, "%s\n", "-ERR ROUTE-DELETE: NetID");
-            } else {
-                if (get_edge(graph, from, to) && get_edge(graph, to, from)) {
-                    remove_edge(graph, from, to);
-                    remove_edge(graph, to, from);
-                    sprintf(write_message, "%s: %d <-> %d\n", "+OK ROUTE-DELETE", from, to);
-                } else {
-                    sprintf(write_message, "%s %d <-> %d %s\n", "-ERR ROUTE-DELETE", from, to, "NOT EXIST");
-                }
-            }
+            route_show(graph, read_message, write_message);
+        } else if (strstr(read_message, COMMAND_NET_ADD)) {
+            net_add(graph, read_message, write_message);
+        } else if (strstr(read_message, COMMAND_NET_DELETE)) {
+            net_delete(graph, read_message, write_message);
+        } else if (strstr(read_message, COMMAND_ROUTE_ADD)) {
+            route_add(graph, read_message, write_message);
+        } else if (strstr(read_message, COMMAND_ROUTE_DELETE)) {
+            route_delete(graph, read_message, write_message);
+        } else if (strstr(read_message, COMMAND_GRAPH_SHOW)) {
+            graph_show(graph, read_message, write_message);
         } else {
-            sprintf(write_message, "%s", "-ERR NOT Implemented\n");
+            sprintf(write_message, "%s", "-ERR: NOT Implemented\n");
         }
         printf("%s\n", write_message);
         //write(clientSocket, write_message, sizeof(write_message));
     }
+    printf("+OK QUIT\n");
     free_graph(graph);
     close(clientSocket);
     close(serverSocket);
